f5917e0f1ab16f7e8fad00ca59ab1133ca9ee0d8
Jonas Baumann <jone@jone.ch>
2011-04-28 09:24:16 +0200

    disabled flymake mode for latex since its really slow on large files

--- private/languages/latex.el
+++ private/languages/latex.el
@@ -6,8 +6,8 @@
 (defun flymake-get-tex-args (file-name)
   (list "chktex" (list "-q" "-v0" file-name)))
 
-(add-hook 'latex-mode-hook
-          '(lambda ()
-             (set-pairs '("(" "{" "[" "\"" "\'"))
-             (auto-fill-mode 1)
-             (flymake-mode t)))
+;; (add-hook 'latex-mode-hook
+;;           '(lambda ()
+;;              (set-pairs '("(" "{" "[" "\"" "\'"))
+;;              (auto-fill-mode 1)
+;;              (flymake-mode t)))
